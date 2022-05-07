d6dd9b1b667c30067a3053d6b53d5e851a87a832
saul-gush <admin@gush.ai>
2022-05-07 11:36:38 +0100

    permissions

--- main.go
+++ main.go
@@ -16,16 +16,36 @@ func main() {
 
 	var testfolder string
 	if os.Getenv("DP_TEST_FOLDER") == "" {
-		testfolder = ""
+		testfolder = "./"
 	} else {
 		testfolder = os.Getenv("DP_TEST_FOLDER")
 	}
 
 	createFile := testfolder + filename
 
+	log.Println("folder:", testfolder)
+	log.Println("file:", createFile)
+
+	/* Folder create */
+	if _, err := os.Stat(testfolder); os.IsNotExist(err) {
+		// path/to/whatever does not exist
+		err := os.MkdirAll(testfolder, os.ModePerm)
+		if err != nil {
+			log.Println(err)
+			time.Sleep(time.Second * 3)
+			panic("😩 File system not ready: folder failure: will exit to retry")
+		}
+		log.Println("Created directory: ", testfolder)
+
+	} else {
+		log.Println("Directory exists: ", testfolder)
+
+	}
+
 	/* Write check. */
 	err := os.WriteFile(createFile, []byte(c), 0644)
 	if err != nil {
+		log.Println(err)
 		time.Sleep(time.Second * 3)
 		panic("😩 File system not ready: write failure: will exit to retry")
 	}
@@ -33,6 +53,7 @@ func main() {
 	/* Read check. */
 	dat, err := os.ReadFile(createFile)
 	if err != nil {
+		log.Println(err)
 		time.Sleep(time.Second * 3)
 		panic("😩 File system not ready: read failure: will exit to retry")
 	}
